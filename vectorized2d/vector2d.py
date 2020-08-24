203f814d526c73e15315d1270b6ae386336b58dd
meirhalachmi <42615120+meirhalachmi@users.noreply.github.com>
2020-08-24 18:55:04 +0300

    rotate -> rotated
    Co-authored-by: mishana <mishana4life@gmail.com>

--- vectorized2d/vector2d.py
+++ vectorized2d/vector2d.py
@@ -74,7 +74,7 @@ class Vector2D(Array2D):
         onto_unit = onto.normalized()
         return self._project_onto(self, onto_unit).view(Vector2D)
 
-    def rotate(self, rotation_angle: Union[float, np.ndarray, Iterable[float]],
+    def rotated(self, rotation_angle: Union[float, np.ndarray, Iterable[float]],
                rotation_units: Units = Units.RADIANS) -> Vector2D:
         if rotation_units is Vector2D.Units.DEGREES:
             rotation_angle = np.deg2rad(rotation_angle)
